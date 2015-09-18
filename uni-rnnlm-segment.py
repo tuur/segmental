40e9389be256a84d32c4cee476cfde7541283df4
tuur <t.leeuwenberg@gmail.com>
2015-09-18 18:47:04 +0200

    Update uni-rnnlm-segment.py

--- uni-rnnlm-segment.py
+++ uni-rnnlm-segment.py
@@ -3,8 +3,8 @@ import argparse
 import subprocess
 import sys
 import os
-
-
+import math
+import shutil
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Given a text file, and a prob-threshold trains a unidirectional RNNLM, and segments the text according to those places where the threshold is reached.')
@@ -18,25 +18,34 @@ if __name__ == '__main__':
                    help='segmented output file (default: segmented.txt)')
     parser.add_argument('-fast', metavar='fast', type=int, default=1,
                    help='Runs each RNNLM only for one iteration, is faster, but the LM is less accurate.(default=1)')
+    parser.add_argument('-hidden', metavar='hidden', type=int, default=50,
+                   help='Number of nodes in the hidden layer of the RNNLMs (default=50)')
 
     args = parser.parse_args()  
     
     if not(os.path.isfile(args.rnnlm)):
         sys.stderr.write('ERROR: Please provide a valid rnnlm path (option: -rnnlm rnnlmFilePath)\n')
         exit()
-    if not(os.path.exists('tmp/')):
-        os.makedirs('tmp/')
      
-    if os.path.exists('tmp/model.tmp'):
-        os.remove('tmp/model.tmp')
+    tmpFolder = args.output + '-tmp/' 
+    
+    if not(os.path.exists(tmpFolder)):
+        os.makedirs(tmpFolder)
+    else:
+        shutil.rmtree(tmpFolder)
+        os.makedirs(tmpFolder)    
+
      
     # Split text in training and development texts, for RNNLM
     sys.stderr.write('(1) Preparing for RNNLM training...\n')
-    trainFile = open('tmp/train.tmp', 'w')
-    devFile = open('tmp/dev.tmp','w')
+    trainFile = open(tmpFolder + 'train.tmp', 'w')
+    devFile = open(tmpFolder + 'dev.tmp','w')
     lineNr = 0
+    vocab = set([])
     with open(args.text) as f: 
         for line in f.readlines():
+            words = line.rstrip().split()
+            vocab = vocab.union(set(words))
             if lineNr%5 == 0:
                 devFile.write(line)
             else:
@@ -45,11 +54,12 @@ if __name__ == '__main__':
     trainFile.close()
     devFile.close()
     
-
+    classSize = int(math.sqrt(len(vocab)))
+    sys.stderr.write('Vocabulary:'+str(len(vocab))+'\n')
     sys.stderr.write('(2) Training RNNLM...\n')
     # Training RNNLM
-    with open('tmp/out.tmp','w') as output:
-        command = [args.rnnlm,'-hidden','20', '-train', 'tmp/train.tmp', '-valid','tmp/dev.tmp','-rnnlm','tmp/model.tmp','-test', args.text,'-debug','2']
+    with open(tmpFolder + 'out.tmp','w') as output:
+        command = [args.rnnlm,'-hidden', str(args.hidden),'-class', str(classSize), '-train', tmpFolder + 'train.tmp', '-valid',tmpFolder + 'dev.tmp','-rnnlm',tmpFolder + 'model.tmp','-test', args.text,'-debug','2']
         if args.fast:
             command += ['-one-iter']
         proc = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE)
